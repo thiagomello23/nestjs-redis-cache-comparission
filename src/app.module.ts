2d0e544e28a5b212c4178f64190f42f58a8ec37e
Thiago mello <thiagomellodiniz@hotmail.com>
2025-02-09 10:36:48 -0300

    feat: Configuring cache system with @nestjs/cache and keyv with redis;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './databases/database.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
+import { Keyv } from 'keyv';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    DatabaseModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          stores: [
+            new Keyv(new KeyvRedis("redis://localhost:6379"))
+          ],
+        };
+      }})
+  ],
   controllers: [],
   providers: [],
 })
